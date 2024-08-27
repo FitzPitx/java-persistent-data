f986b473c20c08a97ccf0be135bce20b6a33740e
Andres Felipe Nieto Jimenez <81382324+FitzPitx@users.noreply.github.com>
2024-08-27 14:51:08 -0500

    initial commit

---
+++ src/main/java/co/edu/ue/persistentdata/PersistentDataApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ue.persistentdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistentDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistentDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/ue/persistentdata/PersistentDataApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ue.persistentdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistentDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
